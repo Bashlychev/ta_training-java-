2a21d0e0d48b035a5bb0bc81123edc4556e8577f
Bashlychev <gbashlychev@gmail.com>
2022-12-28 06:59:54 +0500

    update

--- com.epam.training.student_gleb_bashlychev/src/main/java/com/epam/training/student_gleb_bashlychev/InputStringProgram.java
+++ com.epam.training.student_gleb_bashlychev/src/main/java/com/epam/training/student_gleb_bashlychev/InputStringProgram.java
@@ -1,4 +1,12 @@
 package com.epam.training.student_gleb_bashlychev;
 
+import java.util.Scanner;
+
 public class InputStringProgram {
+    public static void main(String [] args){
+        Scanner scan = new Scanner(System.in);
+        double number = scan.nextDouble();
+        System.out.println(number);
+
+    }
 }
---
+++ com.epam.training.student_gleb_bashlychev/src/main/java/com/epam/training/student_gleb_bashlychev/TryClass.java
@@ -0,0 +1,8 @@
+package com.epam.training.student_gleb_bashlychev;
+
+public class TryClass {
+    static public void main(String[] args) {
+        java.util.Scanner in = new java.util.Scanner(System.in);
+    }
+
+}
